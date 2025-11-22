fc14c8610aedc00b5e91dfccaf03b4399d8bbdbe
eduaardba <mariaeduardaco.200@hotmail.com>
2025-11-22 16:50:59 -0300

    feat: button

---
+++ 2.js
@@ -0,0 +1,15 @@
+
+        let botao = document.getElementById("btn")
+        botao.onclick = () => {
+            let num = prompt("Digite um número!")
+        alert(num + " bazinga")
+        }
+        document.addEventListener("click", () => {
+            alert("botão foi clicado!")
+        })
+
+        document.getElementById("form").addEventListener
+        ("submit", (e) =>{
+            event.preventDefault()
+            console.log()
+        })
